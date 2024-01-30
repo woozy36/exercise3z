a66c45c962a629084ef23ab04cd855b3ae3f9e3e
Joseph S Momot 12th CCHS <117090@SWCSD.US>
2024-01-30 14:04:29 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        userInput();
+
+
+        //while(){
+            //method1 get user input... pass it out
+            //method2 calculate Celsius from f
+            //method3 print out message
+            //ask user for new number? break if under -460}
+
+
+    }
+
+    static void userInput(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("please give me a Fahrenheit: ");
+        int num = input.nextInt();
+        //System.out.println(num);
+    }
+    
+
+
+}
